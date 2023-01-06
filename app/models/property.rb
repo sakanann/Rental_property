b6e89a91f5bdfbd6b15255f089dd8966b60220ab
takuya sakamoto <sthtcwm@yahoo.co.jp>
2023-01-06 14:48:04 +0900

    config-root追記

--- app/models/property.rb
+++ app/models/property.rb
@@ -1,5 +1,4 @@
 class Property < ApplicationRecord
-  root 'properties#index'
   has_many :nearest_stations, dependent: :destroy
   accepts_nested_attributes_for :nearest_stations, reject_if: :all_blank
 

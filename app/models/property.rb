4a1b5a60caf4e3bd86c4179001e17c9c857a580c
takuya sakamoto <sthtcwm@yahoo.co.jp>
2023-01-06 14:27:12 +0900

    バリデーション実装

--- app/models/property.rb
+++ app/models/property.rb
@@ -1,3 +1,6 @@
 class Property < ApplicationRecord
   has_many :nearest_stations, dependent: :destroy
+  accepts_nested_attributes_for :nearest_stations, reject_if: :all_blank
+
+  validates :property_name, :rent, :address, :age, presence: true
 end
